4a94ce2ae9adec5e19b8bd282caac96fb2e6ade2
clarke <git@clarke.io>
2026-04-18 14:52:09 +0100

    added posts index with individual example posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
-
+  get "posts", to: "pages#posts"
+  get "posts/hello_world", to: "pages#hello_world"
+  get "posts/rails_app", to: "pages#rails_app"
   get "up" => "rails/health#show", as: :rails_health_check
 end
